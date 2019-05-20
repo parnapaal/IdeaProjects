397d68a6a72b1ef48e5532d047c15b5f2f32825b
aparna <aparna@bio-fa02son201x.local>
2019-05-19 22:31:19 -0500

    added in documentation for Item.Java

--- ReceiptGenerator/src/com/company/Item.java
+++ ReceiptGenerator/src/com/company/Item.java
@@ -3,6 +3,10 @@ package com.company;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+This class contains our shopping items. Within this class we keep
+item name, its cost, and its total tax
+ */
 public class Item {
 
     private static Map<String, Integer> exempt;
@@ -12,8 +16,7 @@ public class Item {
     private double totalTax;
 
 
-
-
+//instantiate as shopping item
     public Item(String name, double cost) {
 
         buildDict();
@@ -24,6 +27,9 @@ public class Item {
 
     }
 
+    /*set tax based off of a boolean determining if it is exempt
+    (so its candy, popcorn, or coffee), or if it's imported
+     */
     private double setTax(){
         tax = 0.0;
 
@@ -41,32 +47,42 @@ public class Item {
         return tax;
     }
 
+    //if our item isn't exempt, calculate a 10% tax
     private double calculateBaseTax(){
         double baseTax = this.cost * (10.0/100.0);
         baseTax = round(baseTax);
         return baseTax;
     }
 
+    //if our item is imported, calculate a 5% tax
     private double calculateImportTax(){
         double importTax = this.cost * (5.0/100.0);
         importTax = round(importTax);
         return importTax;
     }
 
+    //public method to get the tax amount
     public double getTax(){
 
         //return the tax of the item
         return this.tax;
     }
 
+    //public method to get our string name
     public String getName (){
         return this.name;
     }
 
+    //public method to get the initial cost
     public double getCost () {
         return this.cost;
     }
 
+    /*checks to see if our item is exempt from taxes. Right now this is being
+    done with a dictionary filled with a few keywords ... gets the job done
+    but its not air tight
+    TODO: implement the wiki API to allow items to be searchable
+     */
     private boolean checkBaseTaxForExempt(){
         //check to see if our item is exempt from the 10 percent tax
         //We only need the last word of the item
@@ -81,10 +97,7 @@ public class Item {
         return false;
     }
 
-    private boolean checkImportTax() {
-
-        //check to see if this item is imported
-        /* Instead of checking the first word for "imported" we're going to
+    /* Instead of checking the first word for "imported" we're going to
         check the string. This way even if "imported isn't listed first,
         we can still find it. Unlike looking for tags for the exemptions,
         there's less of a likelihood that just because the word is found,
@@ -94,6 +107,10 @@ public class Item {
         "imported." This still isn't completely fool-proof, but it will do
         for now
          */
+    private boolean checkImportTax() {
+
+        //check to see if this item is imported
+
 
         if(this.name.contains("imported")) {
             return true;
